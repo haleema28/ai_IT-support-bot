7cb3295929b5c05b304782c189858988c8522667
Harshitha-79 <harshithakakubal@gmail.com>
2025-11-02 22:25:24 +0530

    altered App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Dashboard from "./pages/Dashboard";
 import Tickets from "./pages/Tickets";
 import ITDashboard from "./pages/ITDashboard";
 
+import Chatbot from "./pages/Chatbot";
+import KnowledgeBase from "./pages/KnowledgeBase";
+
 export default function App() {
   const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
@@ -19,6 +22,8 @@ export default function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/tickets" element={role === "employee" ? <Tickets /> : <Navigate to="/it" />} />
             <Route path="/it" element={role === "it_support" ? <ITDashboard /> : <Navigate to="/tickets" />} />
+            <Route path="/chat" element={role === "employee"? <Chatbot/> :<Navigate to ="/dashboard" />} />
+            <Route path="/kb" element={role === "it_support"?(<KnowledgeBase/>): (<Navigate to ="/dashboard" />)} />
           </>
         )}
       </Routes>
